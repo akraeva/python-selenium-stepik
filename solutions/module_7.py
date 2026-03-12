53ae18a4e8a1f8dd85c7cde274b1968cbd8705ee
akraeva <adkraeva@gmail.com>
2026-03-12 18:26:00 +0300

    feat: feat: add solutions for module 7 section 2

--- solutions/module_7.py
+++ solutions/module_7.py
@@ -1,5 +1,6 @@
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from main import get_driver
 
 
@@ -15,4 +16,21 @@ def m_7_1_1():
     print(res.split()[-1])  # E7XX-QILL-PWJ1-SE0D
 
 
+def m_7_2_1():
+    with get_driver() as driver:
+        url = "https://parsinger.ru/selenium/7/7.2/index.html"
+        driver.get(url)
+        element = driver.find_element(By.CLASS_NAME, "input-wrapper").find_element(
+            By.TAG_NAME, "input"
+        )
+        for i in range(100):
+            element.send_keys(i)
+            element.send_keys(Keys.ENTER)
+            element.send_keys(Keys.ARROW_DOWN)
+            element = driver.switch_to.active_element
+        res = driver.find_element(By.ID, "hidden-password").text
+    print(res.split()[-1])  # Wasteland-Survivor-2077
+
+
 # m_7_1_1()
+# m_7_2_1()
