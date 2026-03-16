794924efd48e157465b1196a4bfc263b6fc3d494
akraeva <adkraeva@gmail.com>
2026-03-16 18:36:00 +0300

    feat: add solutions for module 8 section 1

---
+++ solutions/module_8.py
@@ -0,0 +1,48 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from main import get_driver
+
+
+def m_8_1_1():
+    with get_driver() as driver:
+        driver.get("about:blank")
+        res = 0
+        data = (
+            {"url": "https://parsinger.ru/selenium/8/8.1/site1/", "to_del": "439"},
+            {"url": "https://parsinger.ru/selenium/8/8.1/site2/", "to_del": "780"},
+        )
+
+        for d in data:
+            driver.switch_to.new_window("tab")
+            driver.get(d["url"])
+            num = "".join(
+                n for n in driver.title if n not in d["to_del"] and n.isdigit()
+            )
+            res += int(num)
+    print(res)  # 4135565044598459771911
+
+
+def m_8_1_2():
+    with get_driver() as driver:
+        url = "https://parsinger.ru/selenium/8/8.1.2/index.html"
+        driver.get(url)
+        first_tab = driver.window_handles[0]
+        urls = [a.get_attribute("href") for a in driver.find_elements(By.TAG_NAME, "a")]
+        res = []
+        for url in urls:
+            driver.switch_to.new_window("tab")
+            driver.get(url)
+            sleep(5)
+            nums = driver.find_element(By.ID, "codePlaceholder").get_attribute(
+                "data-numbers"
+            )
+            res.append(sum(map(int, nums.split(", "))))
+        driver.switch_to.window(first_tab)
+        driver.find_element(By.ID, "sumInput").send_keys(sum(res))
+        driver.find_element(By.ID, "checkButton").click()
+        res = driver.find_element(By.ID, "passwordDisplay").text
+    print(res.split(":")[-1].strip())  # TH3-G4T3S-0F-H3LL-4R3-0P3N
+
+
+# m_8_1_1()
+# m_8_1_2()
